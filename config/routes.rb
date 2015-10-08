7b9037d7afb0b9f1bdf555551b2a9117a4a9a6c1
durech1103 <durech11@gmail.com>
2015-10-08 01:01:53 -0700

    Added Orders page Created a new controller, order_controller.rb Added orders folder inside view with files Edited routes.rb, resources...

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#landing_page'
 
+  resources :orders, only: [:index, :show, :new, :create]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
