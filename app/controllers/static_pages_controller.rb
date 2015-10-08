58ec9c2e6caa8131fa2eabc59877e10ef079f120
durech1103 <durech11@gmail.com>
2015-10-08 00:23:00 -0700

    Added a Featured Page Added landing_page.html.erb. Edited the routes.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  # make a new landing page for featured products
+  def landing_page
+		@featured_product = Product.first
+	end
 end
