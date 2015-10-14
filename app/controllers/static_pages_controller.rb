28704869b7faea4a454cb8db59c8403c5acc4f02
durech1103 <durech11@gmail.com>
2015-10-14 00:14:28 -0700

    Added the Carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,8 @@ class StaticPagesController < ApplicationController
   end
 
   # make a new landing page for featured products
-  def landing_page
-		@featured_product = Product.first
-	end
+def landing_page
+  @products = Product.limit(3)
+end
+
 end
