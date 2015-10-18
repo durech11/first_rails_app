8368750873ee9e1e2692e9d6fa840fbafd55395e
durech1103 <durech11@gmail.com>
2015-10-18 15:51:31 -0700

    Reroute root to about page redirect new products to success page

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
+  get 'static_pages/success'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'static_pages#about'
 
   resources :orders, only: [:index, :show, :new, :create]
 
