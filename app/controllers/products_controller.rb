34d5b8048b0c7edc9b802226a4579e3b841b984d
durech1103 <durech11@gmail.com>
2015-10-18 22:55:48 -0700

    Check if Rails is in Production Edited the products_controller.rb file to check if Rails is in production so that the database in the search box will work properly production - postgresql test - sqlite

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,10 @@ class ProductsController < ApplicationController
     # @products = Product.all
     if params[:q]
       search_term = params[:q]
-      @products = Product.where("name LIKE ?", "%#{search_term}%")
+      if Rails.env.production?
+        @products = Product.where("name ilike ?", "%#{search_term}%")
+      else
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
       # return our filtered list here
     else
       @products = Product.all
