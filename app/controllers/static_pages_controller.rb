c07ad9172d6400820ef298e3070d71d3f979ecf0
durech1103 <durech11@gmail.com>
2015-10-19 23:05:45 -0700

    Added mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,15 @@ def landing_page
 	@products = Product.all
 end
 
+# email controller - development
+def thank_you
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+	ActionMailer::Base.mail(:from => @email,
+		:to => 'crimsonx84@yahoo.com',
+		:subject => "A new contact form message from #{@name}",
+		:body => @message).deliver
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  get 'static_pages/contact'
+  get 'static_pages/contact' => 'static_pages#contact', as: :contact
 
   get 'static_pages/index'
 
   get 'static_pages/landing_page'
 
   get 'static_pages/success'
+
+  post 'static_pages/thank_you'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
